090595f822e140f2e8785a58863b3dcd950488cc
AhmedRezeq8 <59434064+AhmedRezeq8@users.noreply.github.com>
2020-06-02 04:43:54 +0300

    first commit

---
+++ lib/Tools/globals.dart
@@ -0,0 +1,16 @@
+library my_prj.globals;
+
+import 'package:flutter/material.dart';
+
+// Master Color
+Color dark = Color(0xff1E2939);
+String widget_1_title = 'أخبار لبنان';
+
+//
+Color light = Colors.purple[800];
+Color background = Color(0xffeff0f1);
+
+final String baseUrl = "https://alnahdanews.com/api/v1";
+//splash screen
+
+bool isLoged = false;
